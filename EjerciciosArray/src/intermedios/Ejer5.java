c939aa077f49d003a63219e84a147fee1da02cd4
a25davidpf <davpenfp@gmail.com>
2025-10-28 21:15:53 +0100

    Ejercicios 5 y 6 completados

--- EjerciciosArray/src/intermedios/Ejer5.java
+++ EjerciciosArray/src/intermedios/Ejer5.java
@@ -2,13 +2,13 @@ package intermedios;
 
 /**
  * Eliminar duplicados
- *
+ * <p>
  * Crea un array de 10000 números y rellenalo con valores aleatorios entre 0 y 300
- *
+ * <p>
  * Crea un nuevo array que elimine los numeros repetidos del array original
- *
+ * <p>
  * Ejemplo
- *
+ * <p>
  * original: 1, 4, 4, 5
  * nuevo:    1, 4, 5
  */
@@ -16,20 +16,43 @@ package intermedios;
 public class Ejer5 {
 
     public static final int NUM_TOTAL = 10000;
+    public static final int MAX_VALUE = 10000;
 
     public static void main(String[] args) {
 
         int[] listaNumeros = new int[NUM_TOTAL];
+        int[] listaReducida = new int[MAX_VALUE];
+        //Guarda cuantos numeros he añadido a la lista sin duplicado (o lista reducida)
+        int count = 0;
 
-        for (int i = 0; i < listaNumeros.length; i++){
+        for (int i = 0; i < listaNumeros.length; i++) {
 
-            listaNumeros[i] = (int)(0 + 301 * Math.random());
-            System.out.println(listaNumeros[i]);
+            listaNumeros[i] = (int) (0 + 301 * Math.random());
+            System.out.print(listaNumeros[i] + " ");
         }
 
-        
+        System.out.println("==========\n==========");
 
-        //int[] listaReducida = new int[];
+        for (int i = 0; i < listaNumeros.length; i++) {
+            int numeroAComprobar = listaNumeros[i];
+            if(!comprobarIsNumberInArray(numeroAComprobar, listaReducida, count)){
+                listaReducida[count] = numeroAComprobar;
+                count++;
+            }
+
+        }
 
     }
+
+    private static boolean comprobarIsNumberInArray(int numeroAComprobar, int[] listaReducida, int count) {
+        for (int i = 0; i < count; i++) {
+            if (numeroAComprobar == listaReducida[i]){
+                return true;
+            }
+        }
+        return false;
+
+    }
+
+
 }
--- EjerciciosArray/src/intermedios/Ejer6.java
+++ EjerciciosArray/src/intermedios/Ejer6.java
@@ -1,14 +1,40 @@
 package intermedios;
 
+import java.util.Scanner;
+
 /**
  *
  * Preguntale al usuario cuantos alumnos se han presentado al examen.
  *
  * Crea un array con el n de alumnos indicado
  *
- * Crea un array con valores double aleatorios del 0 al 10, este sera el array de notas
+ * Rellena el array  con valores double aleatorios del 0 al 10, este sera el array de notas
  * Muestra la nota media de la clase
  */
 
 public class Ejer6 {
+
+    public static final int POSIBLE_CALIFICATIONS = 11;
+
+    public static void main(String[] args) {
+
+
+        System.out.println("Cuantos alumnos se han presentado al examen?");
+        Scanner sc = new Scanner(System.in);
+        int numberStudents = sc.nextInt();
+
+
+        double[] califications = new double[numberStudents];
+        for (int i = 0; i < califications.length; i++){
+            califications[i] = (0 + 11 * Math.random());
+        }
+
+        double calificationsAll = 0;
+        for (int i = 0; i < califications.length; i++){
+            calificationsAll += califications[i];
+        }
+        double calificationsAvg = calificationsAll / numberStudents;
+
+        System.out.println("La nota media de la clase es " + calificationsAvg);
+    }
 }

47375b40958652fbd8a39d867c564f1d479bd96c
Davpen79 <davpenfp@gmail.com>
2025-10-29 12:34:08 +0100

    Ejercicio 5 bien resuelto

--- EjerciciosArray/src/intermedios/Ejer5.java
+++ EjerciciosArray/src/intermedios/Ejer5.java
@@ -1,5 +1,8 @@
 package intermedios;
 
+import java.util.Arrays;
+import java.util.Random;
+
 /**
  * Eliminar duplicados
  * <p>
@@ -22,37 +25,37 @@ public class Ejer5 {
 
         int[] listaNumeros = new int[NUM_TOTAL];
         int[] listaReducida = new int[MAX_VALUE];
-        //Guarda cuantos numeros he añadido a la lista sin duplicado (o lista reducida)
+        //Guarda cuantos números he añadido a la lista sin duplicado (o lista reducida)
         int count = 0;
 
         for (int i = 0; i < listaNumeros.length; i++) {
 
             listaNumeros[i] = (int) (0 + 301 * Math.random());
-            System.out.print(listaNumeros[i] + " ");
+            System.out.print(listaNumeros[i] + ", ");
         }
 
         System.out.println("==========\n==========");
 
         for (int i = 0; i < listaNumeros.length; i++) {
             int numeroAComprobar = listaNumeros[i];
-            if(!comprobarIsNumberInArray(numeroAComprobar, listaReducida, count)){
+            if (!comprobarIsNumberInArray(numeroAComprobar, listaReducida, count)) {
                 listaReducida[count] = numeroAComprobar;
                 count++;
             }
-
         }
+
+        System.out.print(Arrays.toString(listaReducida));
 
     }
 
     private static boolean comprobarIsNumberInArray(int numeroAComprobar, int[] listaReducida, int count) {
         for (int i = 0; i < count; i++) {
-            if (numeroAComprobar == listaReducida[i]){
+            if (numeroAComprobar == listaReducida[i]) {
                 return true;
             }
         }
         return false;
 
     }
-
 
 }
